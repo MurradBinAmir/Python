dfe3485e86ccf07b7ce79a5600aef3ddf616ef5f
MurradBinAmir <136635900+MurradBinAmir@users.noreply.github.com>
2024-03-21 16:53:41 +0500

    Add files via upload

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,1 +1,11 @@
+# creating a function
+def function():
+    print("I am a Murrad Bin Amir")
+# parameterized function
+def parameter(name):
+    print(f"Hello {name}")
+# calling a function
+function()
 
+first_Name = input("what is your first name : ")
+parameter(first_Name)
